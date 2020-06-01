a135e2c1e4621d9e441b69f7341b3d7e5fd5efca
Лялин Александр <alexandr.l1999@mail.ru>
2020-06-01 22:45:29 +0300

    Added saves

--- src/saves.js
+++ src/saves.js
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
-export default function Saves() {
-    return (
-        <div>
-            <h4>this is saves</h4>
-        </div>
-    );
+class Saves extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            resultStr: window.sessionStorage.getItem('saves')
+        }
+    }
+
+    render() {
+        if(this.state.resultStr === null){
+            return(
+                <div className="wrapper7">
+                    <div className="alert alert-danger">
+                        Пока что тут пусто
+                    </div>
+                </div>
+            )
+        } else {
+            const motocycles = Object.values(JSON.parse(this.state.resultStr));
+            let pageUser = window.sessionStorage.getItem('id');
+            let q = motocycles.length;
+            if(q > 0) {
+                const items = motocycles.map((mot, i) =>
+                    <div className="container" key={i}>
+                        <div className="wrapper6">
+                            <div className="card border-dark mb-3">
+                                <div className="card-header">{mot['mark']} {mot['nameMoto']}</div>
+                                <div className="card-body">
+                                    <h5 className="card-title">{mot['type']}</h5>
+                                    <div>vin: {mot['vin']}</div>
+                                    <div>страна: {mot['country']}</div>
+                                    <div>Год выпуска: {mot['year']}</div>
+                                    <div>Кубатура: {mot['cubature']}</div>
+                                    <div>Владелец: {mot[0]}</div>
+                                    <div>E-mail: {mot[1]}</div>
+                                    <div>Телефон: {mot[2]}</div>
+                                    <div>Город: {mot[3]}</div>
+                                    <div className='dropdown-divider'/>
+                                    <div>Описание: {mot['description']}</div>
+                                    <div className='dropdown-divider'/>
+                                    <div className="small">
+                                        <button className="btn btn-sm btn-outline-secondary">
+                                            <div className="small">Показать историю</div>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+                return (
+                    <div className="wrapper7">
+                        <h4>Список сохраненных обьявлений</h4>
+                        <div className="wrapper7">
+                            <div>{Object.values(items)}</div>
+                        </div>
+                    </div>
+                );
+            } else {
+                return (
+                    <div className="wrapper7">
+                        <h5>Список сохраненных обьявлений</h5>
+                        <div className="alert alert-danger">
+                            Список пуст
+                        </div>
+                    </div>
+                );
+            }
+        }
+    }
 }
+export default Saves
--- src/usernavs.js
+++ src/usernavs.js
@@ -11,13 +11,40 @@ export default function UserNavs() {
             <UserInfo/>
             <ul><Link className="btn btn-outline-secondary btn-block" onClick={handlGarageOnClick} to={`${match.url}/mygarage`}>Мой гараж</Link></ul>
             <ul><Link className="btn btn-outline-secondary btn-block" to={`${match.url}/store`}>Поиск обьявлений</Link></ul>
-            <ul><Link className="btn btn-outline-secondary btn-block" to={`${match.url}/saves`}>Избранные объявления</Link></ul>
+            <ul><Link className="btn btn-outline-secondary btn-block" onClick={handlSavesOnClick} to={`${match.url}/saves`}>Избранные объявления</Link></ul>
             <ul><Link className="btn btn-outline-secondary btn-block" to={`${match.url}/settings`}>Настройки профиля</Link></ul>
             <ul><Link onClick={function () {
                 window.sessionStorage.clear()
             }} className="btn btn-outline-danger btn-block" to={`/loginpage`}>Выйти</Link></ul>
         </div>
     );
+}
+
+async function handlSavesOnClick(){
+    let idUser = window.sessionStorage.getItem('id');
+    let answ = '';
+    try {
+        let resp = await window.fetch("http://localhost/motobase/findUserSaves.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+            },
+            body: new URLSearchParams({
+                iduser: idUser
+            })
+        })
+            .then(response => response.json())
+            .then(result => answ = result)
+        if(answ ==='err row'){
+            alert("Oups thats an error");
+            window.location.assign('/saves');
+        } else {
+            window.location.reload();
+            window.sessionStorage.setItem("saves",JSON.stringify(answ));
+        }
+    } catch (e) {
+        console.error('Ошибка:', e);
+    }
 }
 
 async function handlGarageOnClick() {
